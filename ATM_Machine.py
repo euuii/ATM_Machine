67a756863836bb39147e39da705ac13dba24c812
euuii <bacarisaseuijin@gmail.com>
2025-05-19 18:40:46 +0800

    update

--- ATM_Machine.py
+++ ATM_Machine.py
@@ -3,11 +3,19 @@ def withdraw():
         content = f.read().strip()
         balance = float(content)
         while True:
-            amount = int(input("Enter the amount you want to withdraw: "))
-            if amount <= balance:
-                f.seek(0)
-                balance = balance - amount
-                f.write(str(balance))
-                print(balance)
-                break
+            try:
+                amount = int(input("Enter the amount you want to withdraw: "))
+                if amount % 100 == 0:
+                    if amount <= balance:
+                        f.seek(0)
+                        balance = balance - amount
+                        f.write(str(balance))
+                        f.truncate()
+                        print(balance)
+                        break
+                else:
+                    print("We only accept 100, 500 and 1000 cash")
+            except ValueError:
+                print("Invalid input. Please enter a number.")
+
 withdraw()
