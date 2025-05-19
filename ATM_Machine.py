b15f2b80061bb5f35d3ae3e539b321e6dc009547
euuii <bacarisaseuijin@gmail.com>
2025-05-19 18:15:14 +0800

    final

--- ATM_Machine.py
+++ ATM_Machine.py
@@ -1,2 +1,13 @@
-password = open('password', 'r')
-print(password.read())
+def withdraw():
+    with open("balance", "r+") as f:
+        content = f.read().strip()
+        balance = float(content)
+        while True:
+            amount = int(input("Enter the amount you want to withdraw: "))
+            if amount <= balance:
+                f.seek(0)
+                balance = balance - amount
+                f.write(str(balance))
+                print(balance)
+                break
+withdraw()
