4048bf85c16f41b77e2ed55e92567f36de2a2cd1
euuii <bacarisaseuijin@gmail.com>
2025-05-20 12:42:12 +0800

    update

--- ATM_Machine.py
+++ ATM_Machine.py
@@ -1,23 +1,94 @@
+import os
+
+def balance_reset():
+    with open("balance", "w") as f:
+        f.seek(0)
+        f.write("10000")
+
+def os_clear():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+def os_pause():
+    if os.name == 'nt':
+        os.system('pause')
+    else:
+        input('Press enter to continue...')
+
 def withdraw():
     with open("balance", "r+") as f:
-        content = f.read().strip()
-        balance = float(content)
+        balance = int(f.read().strip())
         while True:
             try:
-                amount = int(input("Enter the amount you want to withdraw: "))
-                if amount % 100 == 0:
+                amount = int(input("Withdraw amount: "))
+                if amount % 100 == 0 and amount > 0:
                     if amount <= balance:
                         f.seek(0)
                         balance = balance - amount
                         f.write(str(balance))
                         f.truncate()
-                        print(f"Your balance is now {balance:.2f}")
+                        print(f"Your balance is now {balance}")
+                        os_pause()
+                        os_clear()
+                        menu()
                         break
                     else:
+                        os_clear()
                         print("Insufficient balance")
                 else:
+                    os_clear()
+                    print("Invalid Amount")
+            except ValueError:
+                os_clear()
+                print("Invalid input. Please enter a valid number.")
+
+def deposit():
+    with open("balance", "r+") as f:
+        balance = int(f.read().strip())
+        while True:
+            try:
+                amount = int(input("Enter the amount you want to deposit: "))
+                if amount % 100 == 0 and amount > 0:
+                    f.seek(0)
+                    balance = balance + amount
+                    f.write(str(balance))
+                    f.truncate()
+                    print(f"Your balance is now {balance}")
+                    os_pause()
+                    os_clear()
+                    menu()
+                    break
+                else:
+                    os_clear()
                     print("We only accept 100, 500 and 1000 cash")
             except ValueError:
-                print("Invalid input. Please enter a number.")
+                os_clear()
+                print("Invalid input. Please enter a valid number.")
 
-withdraw()
+def menu():
+    while True:
+        try:
+            choice = int(input("(1) Withdraw \n(2) Deposit \n(3) Exit \nInput: "))
+            match choice:
+                case 1:
+                    os_clear()
+                    withdraw()
+                    break
+                case 2:
+                    os_clear()
+                    deposit()
+                    break
+                case 3:
+                    exit()
+                case _:
+                    os_clear()
+                    print("Invalid input. Please enter a valid number.")
+        except ValueError:
+            os_clear()
+            print("Invalid input. Please enter a provided input.")
+
+os_clear()
+balance_reset()
+menu()
