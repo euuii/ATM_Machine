367d904e95323fb210fece904bf858413fc8997d
euuii <bacarisaseuijin@gmail.com>
2025-05-20 14:36:08 +0800

    added pin and check balance

--- ATM_Machine.py
+++ ATM_Machine.py
@@ -1,24 +1,55 @@
 import os
 
 def balance_reset():
-    with open("balance", "w") as f:
+    with open("balance.txt", "w") as f:
         f.seek(0)
         f.write("10000")
 
+def verify_pin():
+    while True:
+        try:
+            with open("pin.txt", "r") as f:
+                pin = int(f.readline().strip())
+                log_pin = int(input("Enter PIN: "))
+                if log_pin == pin:
+                    print("PIN verified")
+                    os_pause()
+                    os_clear()
+                    break
+                else:
+                    os_clear()
+                    print("PIN verification failed")
+        except ValueError:
+            os_clear()
+            print("Invalid input, please enter only digits")
+
+def reg_pin():
+    with open("pin.txt", "w") as f:
+        pin = int(input("Enter PIN: "))
+        f.write(str(pin))
+
 def os_clear():
-    if os.name == 'nt':
-        os.system('cls')
+    if os.name == "nt":
+        os.system("cls")
     else:
-        os.system('clear')
+        os.system("clear")
 
 def os_pause():
-    if os.name == 'nt':
-        os.system('pause')
+    if os.name == "nt":
+        os.system("pause")
     else:
-        input('Press enter to continue...')
+        input("Press enter to continue...")
+
+def check_balance():
+    with open("balance.txt", "r") as f:
+        balance = f.readline().strip()
+        print(f"Your balance is: {balance}")
+        os_pause()
+        os_clear()
+        return
 
 def withdraw():
-    with open("balance", "r+") as f:
+    with open("balance.txt", "r+") as f:
         balance = int(f.read().strip())
         while True:
             try:
@@ -29,13 +60,11 @@ def withdraw():
                         balance = balance - amount
                         f.write(str(balance))
                         f.truncate()
-                        print(f"Your balance is now {balance}")
-                        os_pause()
-                        os_clear()
+                        check_balance()
                         return
                     else:
                         os_clear()
-                        print("Insufficient balance")
+                        print("Insufficient balance.txt")
                 else:
                     os_clear()
                     print("Invalid Amount")
@@ -44,7 +73,7 @@ def withdraw():
                 print("Invalid input. Please enter a valid number.")
 
 def deposit():
-    with open("balance", "r+") as f:
+    with open("balance.txt", "r+") as f:
         balance = int(f.read().strip())
         while True:
             try:
@@ -54,13 +83,11 @@ def deposit():
                     balance = balance + amount
                     f.write(str(balance))
                     f.truncate()
-                    print(f"Your balance is now {balance}")
-                    os_pause()
-                    os_clear()
+                    check_balance()
                     return
                 else:
                     os_clear()
-                    print("We only accept 100, 500 and 1000 cash")
+                    print("The only dispensable amount is 1000, 500 and 100")
             except ValueError:
                 os_clear()
                 print("Invalid input. Please enter a valid number.")
@@ -68,15 +95,18 @@ def deposit():
 def menu():
     while True:
         try:
-            choice = int(input("(1) Withdraw \n(2) Deposit \n(3) Exit \nInput: "))
+            choice = int(input("(1) Check Balance \n(2) Withdraw \n(3) Deposit \n(4) Exit \nInput: "))
             match choice:
                 case 1:
                     os_clear()
-                    withdraw()
+                    check_balance()
                 case 2:
                     os_clear()
-                    deposit()
+                    withdraw()
                 case 3:
+                    os_clear()
+                    deposit()
+                case 4:
                     exit()
                 case _:
                     os_clear()
@@ -86,5 +116,8 @@ def menu():
             print("Invalid input. Please enter a provided input.")
 
 os_clear()
-balance_reset()
+# balance_reset()
 menu()
+# reg_pin()
+# verify_pin()
+# check_balance()
